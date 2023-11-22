788be3f54bb33e0b9bccedfbef96b97a0f8df038
omarmaamoun179 <omarmaamoun6@gmail.com>
2023-11-22 20:33:49 +0200

    re build myapp and add func on generate

---
+++ lib/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:black_market/config/router/routes.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: (settings) => AppRoutes.ongenerate(settings),
+    );
+  }
+}
